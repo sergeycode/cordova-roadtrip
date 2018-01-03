b0f58a52784d88d3dd1aa92114be469eeabd9394
sergeycode <s_ovcharenko@outlook.com>
2018-01-03 14:32:42 -0500

    all in one page with static maps and desc

--- platforms/browser/www/js/scripts.js
+++ platforms/browser/www/js/scripts.js
@@ -1,23 +1,18 @@
-//menu
-var menu = document.getElementById('menu');
-var menuLine = document.querySelectorAll('.menu-line');
 //buttons
-var btnMap = document.getElementById('show-map');
-var btnAll = document.getElementById('show-all');
 var save = document.getElementById('save');
-
+//containers
 var mapContainer = document.getElementById('map-container');
 var allPositions = document.getElementById('all');
+//input value
+var desc = document.getElementById('desc');
 //google maps
 var map;
 var marker;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 8,
-        center: {lat: 42.9854526, lng: -81.2449256},
         disableDefaultUI: true
     });
-};
+}
 //firebase
 // Initialize Firebase
 var config = {
@@ -36,8 +31,15 @@ var dbRefMarkers = dbRefTeam.child('markers');
 dbRefMarkers.on('child_added', function (data) {
     var li = document.createElement('li');
     var dbLat = data.val().latitude;
-    var dbLong = data.val().longitude;
-    li.innerHTML = dbLat + '<br>' + dbLong;
+    var dbLng = data.val().longitude;
+    var dbDesc = data.val().desc;
+    //generage map as image
+    dbMap = "http://maps.googleapis.com/maps/api/staticmap?center=" + 
+                dbLat + "," + dbLng + 
+                "&zoom=14&size=" + 120 + "x" + 120 + 
+                "&maptype=roadmap&markers=color:red%7C" +
+                dbLat + "," + dbLng + "&sensor=false";
+    li.innerHTML = '<img class="map-img" src="' + dbMap + '">' + '<p class="map-desc">Description: <br> ' + dbDesc + '</p>';
     li.id = data.key;
     //create li elements with data
     allPositions.appendChild(li);
@@ -49,98 +51,36 @@ dbRefMarkers.on('child_removed', function (data) {
 });
 
 
-//show me on map
-btnMap.addEventListener('click', function() {
-    //hide all positions if visible
-    if (allPositions.style.display === 'block') {
-        allPositions.style.display = 'none';
-    }
-    //animate menu
-    menuLine.forEach( function (e) {
-        e.classList.add('rotate');
-    });
-    //disable this button so you will not place marker again
-    this.disabled = true;
-    //change text color of this button
-    this.style.color = 'rgba(255, 255, 255, 0.4)';
-    btnAll.style.color = 'rgba(255, 255, 255, 0.7)';
-    //show map
-    mapContainer.style.display = 'block';
-    //show button
-    save.style.display = 'block';
+//get current position
+navigator.geolocation.getCurrentPosition(onSuccess, onError, { enableHighAccuracy: true });
+function onSuccess(position) {
+    // coordinates
+    var myCoords = {lat: position.coords.latitude, lng: position.coords.longitude};
+    //set center
+    map.setCenter(myCoords);
+    //marker
+    marker = new google.maps.Marker({
+        position: myCoords
+        });
+    //set zoom
+    map.setZoom(13);
+    //set marker
+    marker.setMap(map);
     
-    //get current position
-    navigator.geolocation.getCurrentPosition(onSuccess, onError, { enableHighAccuracy: true });
-    
-    function onSuccess(position) {
-        // coordinates
-        var myCoords = {lat: position.coords.latitude, lng: position.coords.longitude};
-        //set center
-        map.setCenter(myCoords);
-        //marker
-        marker = new google.maps.Marker({
-            position: myCoords
-          });
-        //set zoom
-        map.setZoom(13);
-        //set marker
-        marker.setMap(map);
-        //save marker to firebase
+    save.addEventListener('click', function () {
+        //get value of textarea
+        var descValue = desc.value;
+        //data object
         var data = {
             latitude: myCoords.lat,
-            longitude: myCoords.lng
-        }
-        save.addEventListener('click', function () {
-            dbRefMarkers.push(data);
-            // clear marker
-            marker.setMap(null);
-            //remove marker
-            marker = null;
-        });
-    };
-    
-    function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-            'message: ' + error.message + '\n');
-    }
-});
-
-btnAll.addEventListener('click', function() {
-    // clear marker
-    if (marker) {
-        marker.setMap(null);
-        //remove marker
-        marker = null;
-    }
-    //enable show map button back
-    btnMap.disabled = false;
-    //change text color of show map button
-    btnMap.style.color = 'rgba(255, 255, 255, 0.7)';
-    //change text color of this button
-    this.style.color = 'rgba(255, 255, 255, 0.4)';
-    //animate menu
-    menuLine.forEach( function (e) {
-        e.classList.add('rotate');
+            longitude: myCoords.lng,
+            desc: descValue
+        };
+        //save data to firebase
+        dbRefMarkers.push(data);
     });
-    //hide map
-    mapContainer.style.display = 'none';
-    //hide button
-    save.style.display = 'none';
-    //show all positions
-    allPositions.style.display = 'block';
-});
-
-
-
-//menu button change from humburger to X
-// menu.addEventListener( 'click', function() {
-//     if (nav.style.visibility === 'hidden') {
-//         menuLine.forEach( function (e) {
-//             e.classList.add('rotate');
-//         });
-//     } else {
-//         menuLine.forEach( function (e) {
-//             e.classList.remove('rotate');
-//         });
-//     }
-// });
+}
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+        'message: ' + error.message + '\n');
+}
